c57c5450bfe52d5eabf50c1f24a6cc04aff37eab
kovalit <kovalit@mail.ru>
2016-07-02 13:38:33 +1000

    ADDED: routes for get list

--- protected/config/_routes.php
+++ protected/config/_routes.php
@@ -2,5 +2,11 @@
 
 return array(
 	'/'                                                                     => 'main/index',
+        '/getSpecialize/'                                                       => 'main/getSpecialize',
+        '/getHospitals/<specializeId:\d+>'                                      => 'main/getHospitals',
+        '/getDoctors/<hospitalId:\d+>/<specializeId:\d+>'                       => 'main/getDoctors',
+    
+    
+        '/booking'                                                              => 'booking/index',
 
 );
